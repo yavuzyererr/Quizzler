c787eed139740a910070c44a3a39606ac9943b38
yavuz yerer <yavuz.yererr@gmail.com>
2023-01-02 15:20:00 +0300

    Start Project

---
+++ Quizzler/Quizzler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Yavuz Yerer on 2.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
